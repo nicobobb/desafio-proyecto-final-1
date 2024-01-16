75b2308a475b765eb765e4bb2a408ef12bc9d728
Nico Bobb <nicobobb@gmail.com>
2024-01-16 06:12:56 -0300

    fix toast

--- js/toast.js
+++ js/toast.js
@@ -4,23 +4,21 @@ function displayToast() {
     toast.id = "toast";
     toast.className = "toast__container border-success w-30";
 
-    // Crear el elemento h4 (título)
     const title = document.createElement("h4");
     title.className = "toast__title";
     title.textContent = "Felicitaciones!";
 
-    // Crear el elemento p (descripción)
     const description = document.createElement("p");
     description.className = "toast__description";
     description.textContent = "Enviamos correctamente tu mensaje";
 
-    // Agregar los elementos al toast en el orden deseado
     toast.appendChild(title);
     toast.appendChild(description);
 
-    // Agregar el toast al cuerpo del documento (o al elemento que desees)
+    // Agrega el toast al cuerpo del documento
     const formSection = document.querySelector(".form__section");
-    formSection.appendChild(toast);
+    const formSectionParent = formSection.parentNode;
+    formSectionParent.insertBefore(toast, formSection);
 
     if (toast.className.includes("fade-out")) {
         toast.className = toast.className.replace("fade-out", "fade-in");
